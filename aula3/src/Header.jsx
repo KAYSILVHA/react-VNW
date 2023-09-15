7942c6af668c73e8694c9b4df2e4624ffe245544
KAYSILVHA <kaysilvha@gmail.com>
2023-09-15 19:29:11 -0300

    first commit

---
+++ aula3/src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./HeaderStyle.css"
+
+const Header = () => {
+  return (
+    <>
+    <header>
+      <nav>
+        <ul className="lista">
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Help</li>
+        </ul>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default Header
